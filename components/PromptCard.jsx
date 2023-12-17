8ca48870e67c9b5f5c146dc3b1365ab743e58d4c
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2023-12-17 14:11:17 +0300

    Profile + prompt func

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import { useSession } from "next-auth/react";
+import { useSession, session } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
@@ -12,7 +12,9 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => setCopied(""), 3000);
   };
-
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -53,6 +55,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         #{post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            onClick={handleEdit}
+            className="font-inter text-sm green_gradient cursor-pointer"
+          >
+            Edit
+          </p>
+          <p
+            onClick={handleDelete}
+            className="font-inter text-sm orange_gradient cursor-pointer"
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
