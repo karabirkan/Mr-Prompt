8e855978d68022d9aead7536b2b4d116e67d6607
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2023-12-18 19:41:47 +0300

    fix: prompt fetching

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,25 +1,22 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+import { headers } from "next/headers";
+
 export const GET = async (request) => {
+  const headersList = headers();
+  const referer = headersList.get("referer");
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-
-    if (prompts.length === 0) {
-      return new Response(JSON.stringify({ message: "No prompts found" }), {
-        status: 404,
-      });
-    }
-
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+      headers: {
+        referer: referer,
+        "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+      },
+    });
   } catch (error) {
-    return new Response(
-      JSON.stringify({
-        error: "Failed to fetch all prompts",
-        details: error.message,
-      }),
-      { status: 500 }
-    );
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
