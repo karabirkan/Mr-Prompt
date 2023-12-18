2f08e7f7d749f2ecb19227f8ce32c3acc946d48e
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2023-12-18 19:14:10 +0300

    Fix: Feed data fetching

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,26 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
 export const GET = async (request) => {
   try {
     await connectToDB();
+
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-    });
+
+    if (prompts.length === 0) {
+      return new Response(JSON.stringify({ message: "No prompts found" }), {
+        status: 404,
+      });
+    }
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    return new Response("Failed to fetch all prompts", { Status: 500 });
+    return new Response(
+      JSON.stringify({
+        error: "Failed to fetch all prompts",
+        details: error.message,
+      }),
+      { status: 500 }
+    );
   }
 };
